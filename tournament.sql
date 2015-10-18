b058b1f3b6b1e321a193a2224c252b9e4ff1921b
Ytry <Kyle12qw@gmail.com>
2015-10-18 19:17:29 -0400

    crated tables
    Created tables for matches and player records

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE matches
+(
+  match_id integer CONSTRAINT matches_pk PRIMARY KEY,
+  player_id integer
+);
 
+CREATE TABLE player_records
+(
+  id integer CONSTRAINT player_record_id PRIMARY KEY,
+  player_id integer,
+  match_id integer REFERENCES matches,
+  outcome text
+);
