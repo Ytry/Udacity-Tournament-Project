bbdadf8b8785c81b8a10cb74d0a82d136bea4ed4
Ytry <Kyle12qw@gmail.com>
2015-10-20 21:45:27 -0400

    formatting

--- tournament_test.py
+++ tournament_test.py
@@ -1,139 +1,115 @@
 #!/usr/bin/env python
 #
-# Test cases for tournament.py
+# tournament.py -- implementation of a Swiss-system tournament
+#
 
-from tournament import *
-
-def testDeleteMatches():
-    deleteMatches()
-    print "1. Old matches can be deleted."
+import psycopg2
 
 
-def testDelete():
-    deleteMatches()
-    deletePlayers()
-    print "2. Player records can be deleted."
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
 
 
-def testCount():
-    deleteMatches()
-    deletePlayers()
-    c = countPlayers()
-    if c == '0':
-        raise TypeError(
-            "countPlayers() should return numeric zero, not string '0'.")
-    if c != 0:
-        raise ValueError("After deleting, countPlayers should return zero.")
-    print "3. After deleting, countPlayers() returns zero."
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("DELETE FROM matches")
+    db.commit()
+    db.close
 
 
-def testRegister():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Chandra Nalaar")
-    c = countPlayers()
-    if c != 1:
-        raise ValueError(
-            "After one player registers, countPlayers() should be 1.")
-    print "4. After registering a player, countPlayers() returns 1."
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("DELETE FROM player_standings")
+    db.commit()
+    cur.execute("DELETE FROM players")
+    db.commit()
+    db.close()
 
 
-def testRegisterCountDelete():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Markov Chaney")
-    registerPlayer("Joe Malik")
-    registerPlayer("Mao Tsu-hsi")
-    registerPlayer("Atlanta Hope")
-    c = countPlayers()
-    if c != 4:
-        raise ValueError(
-            "After registering four players, countPlayers should be 4.")
-    deletePlayers()
-    c = countPlayers()
-    if c != 0:
-        raise ValueError("After deleting, countPlayers should return zero.")
-    print "5. Players can be registered and deleted."
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT count(player_id) FROM players")
+    count = cur.fetchall()
+    db.close()
+    return count[0][0]
 
 
-def testStandingsBeforeMatches():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Melpomene Murray")
-    registerPlayer("Randy Schwartz")
-    standings = playerStandings()
-    if len(standings) < 2:
-        raise ValueError("Players should appear in playerStandings even before "
-                         "they have played any matches.")
-    elif len(standings) > 2:
-        raise ValueError("Only registered players should appear in standings.")
-    if len(standings[0]) != 4:
-        raise ValueError("Each playerStandings row should have four columns.")
-    [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
-    if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0:
-        raise ValueError(
-            "Newly registered players should have no matches or wins.")
-    if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
-        raise ValueError("Registered players' names should appear in standings, "
-                         "even if they have no matches played.")
-    print "6. Newly registered players appear in the standings with no matches."
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+
+    Args:
+      name: the player's full name (need not be unique).
+    """
+
+    db = connect()
+    cur = db.cursor()
+    cur.execute("INSERT INTO players(name) VALUES (%s)", (name,))
+    db.commit()
+    cur.execute("INSERT INTO player_standings (player_id, matches_played, wins) VALUES ((SELECT player_id FROM players where name = %s ),0,0)", (name,))  # noqa
+    db.commit()
+    db.close()
 
 
-def testReportMatches():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Bruno Walton")
-    registerPlayer("Boots O'Neal")
-    registerPlayer("Cathy Burton")
-    registerPlayer("Diane Grant")
-    standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    standings = playerStandings()
-    for (i, n, w, m) in standings:
-        if m != 1:
-            raise ValueError("Each player should have one match recorded.")
-        if i in (id1, id3) and w != 1:
-            raise ValueError("Each match winner should have one win recorded.")
-        elif i in (id2, id4) and w != 0:
-            raise ValueError("Each match loser should have zero wins recorded.")
-    print "7. After a match, players have updated standings."
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player # noqa
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT players.player_id, name, wins, matches_played FROM players, player_standings WHERE players.player_id = player_standings.player_id ORDER BY wins desc")  # noqa
+    standings = cur.fetchall()
+    return standings
 
 
-def testPairings():
-    deleteMatches()
-    deletePlayers()
-    registerPlayer("Twilight Sparkle")
-    registerPlayer("Fluttershy")
-    registerPlayer("Applejack")
-    registerPlayer("Pinkie Pie")
-    standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    pairings = swissPairings()
-    if len(pairings) != 2:
-        raise ValueError(
-            "For four players, swissPairings should return two pairs.")
-    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4)] = pairings
-    correct_pairs = set([frozenset([id1, id3]), frozenset([id2, id4])])
-    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
-    if correct_pairs != actual_pairs:
-        raise ValueError(
-            "After one match, players with one win should be paired.")
-    print "8. After one match, players with one win are paired."
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("INSERT INTO matches (w_player_id, l_player_id) VALUES (%s, %s)", (winner, loser,))  # noqa
+    db.commit()
+    cur.execute("UPDATE player_standings SET matches_played = matches_played+1, wins = wins+1 WHERE player_standings.player_id = %s ", (winner,))  # noqa
+    db.commit()
+    cur.execute("UPDATE player_standings SET matches_played = matches_played+1 WHERE player_standings.player_id = %s ", (loser,))  # noqa
+    db.commit()
+    db.close()
 
 
-if __name__ == '__main__':
-    testDeleteMatches()
-    testDelete()
-    testCount()
-    testRegister()
-    testRegisterCountDelete()
-    testStandingsBeforeMatches()
-    testReportMatches()
-    testPairings()
-    print "Success!  All tests pass!"
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
 
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
 
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
